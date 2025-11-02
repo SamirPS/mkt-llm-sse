8c375e2df5d4772496b7a8338075b7343a2f656d
slandelle <slandelle@gatling.io>
2025-11-02 22:13:01 +0100

    fix: don't ship the maven local cache in the git repository

--- articles/load-test-llm-sse/mvnw.cmd
+++ articles/load-test-llm-sse/mvnw.cmd
@@ -1,6 +1,3 @@
-set SCRIPT_PATH=%~dp0
-set MAVEN_USER_HOME=%SCRIPT_PATH%.mvn
-set REPO_HOME=%SCRIPT_PATH%.m2\\repository
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
